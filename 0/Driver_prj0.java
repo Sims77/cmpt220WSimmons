0a5c1676ccc0363c09bfec7cedf027ffe77e7489
Sims77 <William.Simmons1@marist.edu>
2017-01-24 17:32:34 -0500

    Add files via upload

---
+++ 0/Driver_prj0.java
@@ -0,0 +1,18 @@
+/**
+  * file: Driver_prj0.java
+  * author: William Simmons
+  * course: CMPT 220
+  * assignment: project 0
+  * due date: January 24, 2017
+  * version: 1
+  * 
+  * This file contains the output of 
+  * Hello World! that is a common simple
+  * java program.
+  */
+public class Driver_prj0 {
+  public static void main(String[] args) {
+    // This will display the message Hello World!
+      System.out.println("Hello World!");
+  }
+}
